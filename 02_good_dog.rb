f9a9543a4bc88e15e55e2470f732a4cf7aeea096
Junsung Park <zeipar@gmail.com>
2020-04-28 23:59:48 +0000

    added getter and setter for name

--- 02_good_dog.rb
+++ 02_good_dog.rb
@@ -6,6 +6,14 @@ class GoodDog
     @name = name
   end
 
+  def get_name
+    @name
+  end
+
+  def set_name=(name)
+    @name = name
+  end
+  
   def speak
     "#{@name} says Arf!"
   end
@@ -13,6 +21,9 @@ end
 
 sparky = GoodDog.new("Sparky")
 puts sparky.speak
+puts sparky.get_name
+sparky.set_name = "Twice"
+puts sparky.get_name
 
 fido = GoodDog.new("Fido")
 puts fido.speak
