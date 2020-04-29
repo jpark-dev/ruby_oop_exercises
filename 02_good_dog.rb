747b7e0e353f77b2f9c09e4acf6e27bb83e8d08f
Junsung Park <zeipar@gmail.com>
2020-04-29 00:42:25 +0000

    Calling methods with self

--- 02_good_dog.rb
+++ 02_good_dog.rb
@@ -1,24 +1,36 @@
 class GoodDog
   # attr_reader / attr_writer can be used if just getter/setter is needed.
-  attr_accessor :name
+  attr_accessor :name, :height, :weight
 
   # constructor
-  def initialize(name)
+  def initialize(n, h, w)
 
     # @variable = instance variable
-    @name = name
+    @name = n
+    @height = h
+    @weight = w
   end
   
   def speak
     "#{name} says Arf!"
   end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{self.name} weighs #{self.weight} and is #{self.height} tall."
+  end
 end
 
-sparky = GoodDog.new("Sparky")
-puts sparky.speak
-puts sparky.name
-sparky.name = "Twice"
-puts sparky.name
+sparky = GoodDog.new("Sparky", "12 inches", "10 lbs")
+puts sparky.info
+puts sparky.change_info("Happy", "2 inches", "5 lbs")
+puts sparky.info
 
-fido = GoodDog.new("Fido")
-puts fido.speak
+# fido = GoodDog.new("Fido")
+# puts fido.speak
+
