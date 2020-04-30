097325461800a74606d19c32f4a9b3ab2199bf49
Junsung Park <zeipar@gmail.com>
2020-04-30 06:06:36 +0000

    method overriding

--- 06_Inheritance.rb
+++ 06_Inheritance.rb
@@ -5,13 +5,22 @@ class Animal
 end
 
 class GoodDog < Animal
+  attr_accessor :name
+
+  def initialize(n)
+    self.name = n
+  end
+
+  def speak
+    "#{self.name} says arf!"
+  end
 end
 
 class Cat < Animal
 end
 
 
-sparky = GoodDog.new
+sparky = GoodDog.new("Sparky")
 paw = Cat.new
 puts sparky.speak
 puts paw.speak
