c20848290ce1d3e12cd1aca861007baf702c7330
Junsung Park <zeipar@gmail.com>
2020-04-30 06:13:34 +0000

    Another way of using super with arguments

--- 07_Inheritance_super.rb
+++ 07_Inheritance_super.rb
@@ -1,14 +1,36 @@
 class Animal
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+
   def speak
     "Hello!"
   end
 end
 
 class GoodDog < Animal
+  def initialize(color)
+    super
+    @color = color
+  end
+  
   def speak
     super + " from GoodDog class"
   end
 end
 
-sparky = GoodDog.new
-sparky.speak        # => "Hello! from GoodDog class"
+# sparky = GoodDog.new
+# sparky.speak        # => "Hello! from GoodDog class"
+
+bruno = GoodDog.new("brown");
+
+class BadDog < Animal
+  def initialize(age, name)
+    super(name)
+    @age = age
+  end
+end
+
+BadDog.new(2, "bear")   
